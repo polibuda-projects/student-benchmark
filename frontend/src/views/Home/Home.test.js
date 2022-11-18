5910d3a5675f49791381523b3aa9f5010ef55ffc
Zoltw <filip.zolyniak@student.pk.edu.pl>
2022-11-18 13:19:30 +0100

    f added rest of files

--- frontend/src/views/Home/Home.test.js
+++ frontend/src/views/Home/Home.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './Home';
+import Home from './Home';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Home />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
