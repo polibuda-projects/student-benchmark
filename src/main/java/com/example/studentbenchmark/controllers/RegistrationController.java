9141c4fb56d70f7888f18b95b122268fe7cb6e9c
Nikodem Zbaraza <nikozet@interia.pl>
2022-11-20 20:35:57 +0100

    b added annotations @Service and @RestController

--- src/main/java/com/example/studentbenchmark/controllers/RegistrationController.java
+++ src/main/java/com/example/studentbenchmark/controllers/RegistrationController.java
@@ -7,7 +7,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 
+
+@RestController("/")
+@Service
 public class RegistrationController {
     private UserRepo userRepo;
 
