9c728b77e61f4cf0270a172b9bebf79dae132854
Eve0028 <sylwia14082@gmail.com>
2022-11-24 00:55:14 +0100

    f added Input component

---
+++ frontend/src/components/Input/Input.tsx
@@ -0,0 +1,25 @@
+import { Component, HTMLAttributes } from 'react';
+
+import style from './Input.module.css';
+
+
+export interface InputProps {
+  type?: string,
+  name?: string,
+  placeholder?: string,
+  shadows?: boolean,
+  className?: string
+}
+
+export default class ContainerBox extends Component<InputProps> {
+  private static defaultProps: InputProps = {
+    shadows: true,
+  };
+
+  render() {
+    return (
+      <input className={[style.input, this.props.className, this.props.shadows ? style.shadow : ''].join(' ')}
+        type={this.props.type} name={this.props.name} placeholder={this.props.placeholder}/>
+    );
+  }
+}
