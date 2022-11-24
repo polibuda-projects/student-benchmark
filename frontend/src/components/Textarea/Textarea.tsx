0b12f782089daea673058d79c5752bf011e95461
PonuryZniwiarz2k <karolwojtasz@gmail.com>
2022-11-24 14:35:34 +0100

    f added support desktop page and Textarea component

---
+++ frontend/src/components/Textarea/Textarea.tsx
@@ -0,0 +1,25 @@
+import { Component, HTMLAttributes } from 'react';
+
+import style from './Textarea.module.css';
+
+
+export interface TextareaProps {
+    type?: string,
+    name?: string,
+    placeholder?: string,
+    shadows?: boolean,
+    className?: string
+}
+
+export default class ContainerBox extends Component<TextareaProps> {
+  private static defaultProps: TextareaProps = {
+    shadows: true,
+  };
+
+  render() {
+    return (
+      <textarea className={[style.textarea, this.props.className, this.props.shadows ? style.shadow : ''].join(' ')}
+        name={this.props.name} placeholder={this.props.placeholder} />
+    );
+  }
+}
--- frontend/src/views/Support/Support.js
+++ frontend/src/views/Support/Support.js
@@ -1,12 +1,32 @@
 import style from './Support.module.css';
-
+import ContainerBox from '@components/ContainerBox/ContainerBox';
+import Input from '@components/Input/Input';
+import Textarea from '@components/Textarea/Textarea';
 import Page from '@components/Page/Page';
+import ButtonMedium from '@components/Buttons/ButtonMedium';
 
 
 function Support() {
   return (
-    <Page title='Support'>
-      <span className={style.todo}>Support Page Placeholder</span>
+    <Page>
+      <ContainerBox className={style.container} width={'100%'}>
+        <h1 className={style.title}>Support - contact us</h1>
+        <form method="post" action="#" className={style.form}>
+          <Input required={true} type={'email'} name={'emailSupport'} placeholder={'Email'} className={style.formElement} />
+          <Input required={true} type={'text'} name={'titleSupport'} placeholder={'Title'} className={style.formElement} />
+          <Textarea required={true} name={'textSupport'} placeholder={'Your message'} className={style.textElement} />
+
+          <label className={style.label}>
+            <input type="checkbox" name="terms" value="terms" required={true} />
+            <em>I agree to our <a className={style.tosLink} href={'/tos'}>terms and privacy of sevice.</a></em>
+          </label>
+
+          <div className={style.formOptions}>
+            <ButtonMedium text={'Send'} />
+          </div>
+
+        </form>
+      </ContainerBox>
     </Page>
   );
 }
