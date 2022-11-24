70c0d49a1c45e4d3109a6f54b43f1d0eba24561f
tsebes <sebastian.zych@student.pk.edu.pl>
2022-11-24 23:32:00 +0100

    b fixed adding support controller and mail sender

--- src/main/java/com/example/studentbenchmark/controllers/SupportController.java
+++ src/main/java/com/example/studentbenchmark/controllers/SupportController.java
@@ -1,2 +1,43 @@
-package com.example.studentbenchmark.controllers;public class SupportController {
+package com.example.studentbenchmark.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
+
+
+@RestController
+@Service
+public class SupportController {
+    private final JavaMailSender emailSender;
+
+    @Autowired
+    public SupportController(JavaMailSender emailSender){
+        this.emailSender = emailSender;
+    }
+
+    @PostMapping("/support")
+    public ResponseEntity<String> sendSupport(@RequestBody SupportRequest request) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(request.senderEmail());
+        message.setTo(request.recieverEmail);
+        message.setSubject(request.subject);
+        message.setText(request.body());
+        try{
+            emailSender.send(message);
+        }
+        catch(MailException e){
+            return new ResponseEntity<>("Email send failed", HttpStatus.EXPECTATION_FAILED);
+        }
+        return new ResponseEntity<>("Email send successfully", HttpStatus.OK);
+    }
+    private record SupportRequest(String senderEmail, String recieverEmail, String subject, String body) {
+    }
+
 }
--- src/main/java/com/example/studentbenchmark/entity/MailSender.java
+++ src/main/java/com/example/studentbenchmark/entity/MailSender.java
@@ -1,2 +1,30 @@
-package com.example.studentbenchmark.entity;public class MailSender {
+package com.example.studentbenchmark.entity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import javax.persistence.Entity;
+import java.util.Properties;
+
+@Entity
+public class MailSender {
+    @Bean
+    public JavaMailSender getJavaMailSender()
+    {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(25);
+
+        mailSender.setUsername("studentbenchmark.noreply.gmail.com");
+        mailSender.setPassword("StudentBenchmark");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
 }
