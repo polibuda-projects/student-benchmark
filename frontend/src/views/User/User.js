4d7a1348a3f45855c87baffac2679e6f39d01be3
Konrad Wojtowicz <konrad.wojtowicz@interia.pl>
2022-11-25 09:25:59 +0100

    f added user page

--- frontend/src/views/User/User.js
+++ frontend/src/views/User/User.js
@@ -1,12 +1,28 @@
 import style from './User.module.css';
-
-import Page from '@components/Page/Page';
-
+import Page from '@components/Page/PageWithoutUser';
+import ContainerBox from '@components/ContainerBox/ContainerBox';
+import ButtonMedium from '@components/Buttons/ButtonMedium';
+import ButtonGrey from '@components/Buttons/ButtonGrey';
+import { Link } from 'react-router-dom';
+import userAvatar from '../../components/UserProfile/defaultAvatar.svg';
 
 function User() {
   return (
-    <Page title='User'>
-      <span className={style.todo}>User Page Placeholder</span>
+    <Page>
+      <ContainerBox width={'100%'}>
+        <img src={userAvatar} className={style.userAvatar} alt={'Default Avatar'} />
+        <div className={style.userBar}>
+          <span className={style.username}>UserWithLongUsername</span>
+        </div>
+        <div className={style.buttons}>
+          <ButtonGrey text={'Dashboard'} width={'32rem'}/>
+          <Link to='/password'>
+            <ButtonGrey text={'Change Password'} width={'32rem'}/>
+          </Link>
+          <ButtonGrey text={'Delete Account'} width={'32rem'}/>
+        </div>
+        <ButtonMedium text={'Log Out'} width={'25rem'}/>
+      </ContainerBox>
     </Page>
   );
 }
