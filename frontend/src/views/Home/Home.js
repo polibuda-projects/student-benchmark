70686514a62b57c0cefe01eb8091befa3f05f625
Zoltw <filip.zolyniak@student.pk.edu.pl>
2022-11-25 23:40:18 +0100

    f fixed scale issues and home description

--- frontend/src/views/Home/Home.js
+++ frontend/src/views/Home/Home.js
@@ -11,7 +11,7 @@ function Home() {
     <Page title='Welcome'>
       <img src={logo} className={style.logo} alt='Student Benchmark' />
 
-      <span className={style.catchphrase}>Lorem ipsum, one liners are hard</span>
+      <span className={style.catchphrase}>Challange your memory and cognitive skills.</span>
 
       <Link to='/home'>
         <ButtonMedium className={style.getStartedButton} text='Get started' />
--- frontend/src/views/TestsHome/TestsHome.js
+++ frontend/src/views/TestsHome/TestsHome.js
@@ -13,25 +13,27 @@ import Page from '@components/Page/Page';
 function TestsHome() {
   return (
     <Page title='Welcome'>
-      <div className={style.rowTests}>
-        <div className={style.nullButton} div/>
-        <Link className={style.testButton} to='/#'>
-          <TestButton text='sequence memory' src={sequence} alt='sequence memory'/>
-        </Link>
-        <Link className={style.testButton} to='/#'>
-          <TestButton text='visual memory' src={visual} alt='visual memory'/>
-        </Link>
-        <div className={style.nullButton} />
-      </div>
-      <div className={style.rowTests}>
-        <div className={style.nullButton} />
-        <Link className={style.testButton} to='/#'>
-          <TestButton text='verbal memory' src={verbal} alt='verbal memory'/>
-        </Link>
-        <Link className={style.testButton} to='/#'>
-          <TestButton text='number memory' src={number} alt='number memory'/>
-        </Link>
-        <div className={style.nullButton} div/>
+      <div className={style.blocks}>
+        <div className={style.rowTests}>
+          <div className={style.nullButton} div/>
+          <Link className={style.testButton} to='/#'>
+            <TestButton text='sequence memory' src={sequence} alt='sequence memory'/>
+          </Link>
+          <Link className={style.testButton} to='/#'>
+            <TestButton text='visual memory' src={visual} alt='visual memory'/>
+          </Link>
+          <div className={style.nullButton} />
+        </div>
+        <div className={style.rowTests}>
+          <div className={style.nullButton} />
+          <Link className={style.testButton} to='/#'>
+            <TestButton text='verbal memory' src={verbal} alt='verbal memory'/>
+          </Link>
+          <Link className={style.testButton} to='/#'>
+            <TestButton text='number memory' src={number} alt='number memory'/>
+          </Link>
+          <div className={style.nullButton} div/>
+        </div>
       </div>
     </Page>
   );
