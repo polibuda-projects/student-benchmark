fdbd8dffe8255cf4cf7d0f3f3df2e14b9e5ab1b9
jkbwtk <jakub.wiatek@student.pk.edu.pl>
2022-11-29 13:10:48 +0100

    f imporoved sidebar

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,7 @@
 import style from './Sidebar.module.css';
 import logo from './logo.svg';
+import icon from './icon.svg';
+import leftDoubleArrow from './leftDoubleArrow.svg';
 
 import homeIcon from './homeIcon.svg';
 import dashboardIcon from './dashboardIcon.svg';
@@ -7,56 +9,79 @@ import supportIcon from './supportIcon.svg';
 import donateBoxIcon from './donateBoxIcon.svg';
 
 import MediumButton from '@components/Buttons/ButtonMedium';
-import { Link, NavigateFunction, NavLink, useNavigate } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { Component, HTMLAttributes } from 'react';
 
 
-export interface SidebarProps {
-  navigate: NavigateFunction;
+export interface SidebarProps {}
+
+export interface SidebarState {
+  active: boolean;
+  floating: boolean;
 }
 
-export class SidebarComponent extends Component<SidebarProps> {
+export default class SidebarComponent extends Component<SidebarProps, SidebarState> {
+  private static staticActive = true;
+
+  constructor(props: SidebarProps) {
+    super(props);
+
+    this.state = {
+      active: SidebarComponent.staticActive,
+      floating: false,
+    };
+  }
+
   render() {
     return (
-      <div className={style.sidebar}>
-        <Link className={style.logo} to='/'>
-          <img src={logo} alt='Student Benchmark' />
-        </Link>
+      <div className={this.dummyContainerClasses}>
+        <div className={style.container}>
+          <div className={style.sidebar}>
+            <Link className={style.branding} to='/#'>
+              <img src={logo} className={style.logo} alt='Student Benchmark' />
+              <img src={icon} className={style.icon} alt=''/>
+            </Link>
 
-        <div className={style.menu}>
-          <NavLink className={style.menuButton} to='/'>
-            <div style={SidebarComponent.menuButtonStyleGenerator(homeIcon)} />
-            <span>Home</span>
-          </NavLink>
+            <div className={style.menu}>
+              <NavLink className={style.menuButton} to='/'>
+                <div style={SidebarComponent.menuButtonStyleGenerator(homeIcon)} />
+                <span>Home</span>
+              </NavLink>
 
-          <NavLink className={style.menuButton} to='/dashboard'>
-            <div style={SidebarComponent.menuButtonStyleGenerator(dashboardIcon)} />
-            <span>Dashboard</span>
-          </NavLink>
+              <NavLink className={style.menuButton} to='/dashboard'>
+                <div style={SidebarComponent.menuButtonStyleGenerator(dashboardIcon)} />
+                <span>Dashboard</span>
+              </NavLink>
 
-          <NavLink className={style.menuButton} to='/support'>
-            <div style={SidebarComponent.menuButtonStyleGenerator(supportIcon)} />
-            <span>Support</span>
-          </NavLink>
+              <NavLink className={style.menuButton} to='/support'>
+                <div style={SidebarComponent.menuButtonStyleGenerator(supportIcon)} />
+                <span>Support</span>
+              </NavLink>
 
-          <NavLink className={style.menuButton} to='/donate'>
-            <div style={SidebarComponent.menuButtonStyleGenerator(donateBoxIcon)} />
-            <span>Donate</span>
-          </NavLink>
+              <NavLink className={style.menuButton} to='/donate'>
+                <div style={SidebarComponent.menuButtonStyleGenerator(donateBoxIcon)} />
+                <span>Donate</span>
+              </NavLink>
 
-        </div>
+            </div>
 
-        <div className={style.auth}>
-          <MediumButton text='Login' width='100%' onClick={() => this.props.navigate('/login')} />
-          <MediumButton text='Sign up' width='100%' onClick={() => this.props.navigate('/signup')} />
-        </div>
+            <div className={style.auth}>
+              <Link to='/login'><MediumButton text='Login' width='100%'/></Link>
+              <Link to='/signup'><MediumButton text='Sign up' width='100%'/></Link>
+            </div>
 
-        <div className={style.links}>
-          <Link className={style.link} to='/privacy'>Privacy</Link>
-          <div className={style.linkDivider}></div>
-          <Link className={style.link} to='/tos'>Terms of service</Link>
-          <div className={style.linkDivider}></div>
-          <Link className={style.link} to='/github'>Github</Link>
+            <div className={style.links}>
+              <Link className={style.link} to='/privacy'>Privacy</Link>
+              <div className={style.linkDivider}></div>
+              <Link className={style.link} to='/tos'>Terms of service</Link>
+              <div className={style.linkDivider}></div>
+              <Link className={style.link} to='/github'>Github</Link>
+            </div>
+
+          </div>
+
+          <img className={style.burger} src={leftDoubleArrow} onClick={this.toggleSidebar} alt='?'/>
+          <div className={style.shadowBox}></div>
         </div>
       </div>
     );
@@ -68,12 +93,52 @@ export class SidebarComponent extends Component<SidebarProps> {
       WebkitMaskImage: `url(${icon})`,
     };
   };
-}
 
-export default function Sidebar() {
-  const navigate = useNavigate();
+  private joinClasses(...classes: string[]) {
+    return classes.join(' ');
+  }
 
-  return (
-    <SidebarComponent navigate={navigate}/>
-  );
+  private toggleSidebar = () => {
+    SidebarComponent.staticActive = !this.state.active;
+    this.setState({ active: SidebarComponent.staticActive });
+  };
+
+  private get dummyContainerClasses() {
+    return this.joinClasses(style.dummyContainer, this.state.active ? '':style.sidebarThin, this.state.floating ? style.sidebarFloating : '');
+  }
+
+  public componentDidMount() {
+    this.registerEventListeners();
+    this.resizeViewer();
+  }
+
+  public componentDidUpdate() {
+    this.unregisterEventListeners();
+    this.registerEventListeners();
+  }
+
+  public componentWillUnmount() {
+    this.unregisterEventListeners();
+  }
+
+  private registerEventListeners() {
+    window.addEventListener('resize', this.resizeViewer);
+  }
+
+  public unregisterEventListeners(): void {
+    window.removeEventListener('resize', this.resizeViewer);
+  }
+
+  private resizeViewer = () => {
+    if (window.innerWidth < 800) this.setState({ floating: true });
+    else this.setState({ floating: false });
+
+    if (window.innerWidth < 1024) {
+      // wrapping in timeout makes it animate on route change
+      setTimeout(() => {
+        this.setState({ active: false });
+        SidebarComponent.staticActive = false;
+      }, 1);
+    }
+  };
 }
