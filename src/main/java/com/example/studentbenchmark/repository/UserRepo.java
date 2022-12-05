158a1ab9eb509c8c16de543c9b8c04f28aeb66a5
wojzam <wojciech.zamarski@student.pk.edu.pl>
2022-12-05 13:10:04 +0100

    b fixed UserRepo sql queries

--- src/main/java/com/example/studentbenchmark/repository/UserRepo.java
+++ src/main/java/com/example/studentbenchmark/repository/UserRepo.java
@@ -17,20 +17,21 @@ public interface UserRepo extends JpaRepository<AppUser, Long> {
 
 
     AppUser findByEmail(String email);
-    @Query(nativeQuery = true, value= "SELECT * FROM App_User u WHERE u.email= :email AND u.password= :password")
-    AppUser findUser(@Param("email") String email, @Param("password")String password);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM App_User u WHERE u.email= :email AND u.password= :password")
+    AppUser findUser(@Param("email") String email, @Param("password") String password);
 
 
-    @Query(nativeQuery = true, value= "SELECT * FROM App_User u WHERE u.idUser= :idUser")
+    @Query(nativeQuery = true, value = "SELECT * FROM App_User u WHERE u.id_user= :idUser")
     AppUser findByID(@Param("idUser") Long idUser);
 
     @Transactional
     @Modifying
-    @Query(nativeQuery = true, value= "UPDATE App_User u SET u.password= :newPassword WHERE u.idUser= :idUser")
-    void changeUserPassword(@Param("idUser") Long idUser, @Param("newPassword")String newPassword);
+    @Query(nativeQuery = true, value = "UPDATE App_User u SET u.password= :newPassword WHERE u.id_user= :idUser")
+    void changeUserPassword(@Param("idUser") Long idUser, @Param("newPassword") String newPassword);
 
     @Transactional
     @Modifying
-    @Query(nativeQuery = true, value= "DELETE FROM App_User u WHERE u.email = :email;")
+    @Query(nativeQuery = true, value = "DELETE FROM App_User u WHERE u.email = :email;")
     void deleteAccount(@Param("email") String email);
 }
