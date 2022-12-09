3d363f136b69191dc1597cbccc1b7f60c2595fa3
jkbwtk <jakub.wiatek@student.pk.edu.pl>
2022-12-09 14:54:37 +0100

    f simplified home navigation

--- frontend/src/views/Home/Home.js
+++ frontend/src/views/Home/Home.js
@@ -4,9 +4,15 @@ import logo from './logo.svg';
 import ButtonMedium from '@components/Buttons/ButtonMedium';
 import Page from '@components/Page/Page';
 import { Link } from 'react-router-dom';
+import TestsHome from '@views/Tests/Home/Home';
 
+let visited = false;
 
 function Home() {
+  if (visited) {
+    return <TestsHome />;
+  }
+
   return (
     <Page title='Welcome'>
       <img src={logo} className={style.logo} alt='Student Benchmark' />
@@ -14,7 +20,7 @@ function Home() {
       <span className={style.catchphrase}>Challange your memory and cognitive skills.</span>
 
       <Link to='/tests'>
-        <ButtonMedium className={style.getStartedButton} text='Get started' />
+        <ButtonMedium className={style.getStartedButton} text='Get started' onClick={() => visited = true} />
       </Link>
     </Page>
   );
