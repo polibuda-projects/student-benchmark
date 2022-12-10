b9652a4cb4cbfd5546125700c59b25d9f9f26088
jkbwtk <jakub.wiatek@student.pk.edu.pl>
2022-12-10 21:38:02 +0100

    f configured docker compose

---
+++ docker/backend/docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "spring.jpa.hibernate.ddl-auto=create" > application.properties
+echo "spring.datasource.url=jdbc:mysql://$DB_HOST:$DB_PORT/$DB_NAME" >> application.properties
+
+echo "spring.datasource.username=$MYSQL_USER" >> application.properties
+echo "spring.datasource.password=$MYSQL_PASSWORD" >> application.properties
+
+echo "spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver" >> application.properties
+
+
+echo "Waiting for the MySQL server to start on $DB_HOST:$DB_PORT"
+
+while ! nc -z $DB_HOST $DB_PORT; do
+  sleep 0.2
+done
+
+echo "MySQL Server has started"
+
+java -jar app.jar
