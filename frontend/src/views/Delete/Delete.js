c31b682c0fe3181df24d8471093d0d147d94ad2b
Mateusz Świstak <swistakmatt@gmail.com>
2022-12-11 03:45:06 +0100

    f added input validation

--- frontend/src/views/Delete/Delete.js
+++ frontend/src/views/Delete/Delete.js
@@ -4,16 +4,26 @@ import Page from '@components/Page/Page';
 import ContainerBox from '@components/ContainerBox/ContainerBox';
 import Input from '@components/Input/Input';
 import ButtonMedium from '@components/Buttons/ButtonMedium';
-
+import { useState } from 'react';
 
 function Delete() {
+  const [isShown, setIsSHown] = useState(false);
+
+  const togglePassword = () => {
+    setIsSHown((isShown) => !isShown);
+  };
+
   return (
     <Page title='Delete'>
       <section className={style.section}>
         <ContainerBox width={'60em'}>
           <h1 className={style.title}>Enter your password</h1>
           <form method="post" action="#" className={style.form}>
-            <Input className={style.formElement} type={'password'} placeholder={'Password'} />
+            <Input type={isShown ? 'text' : 'password'} name={'passwordLogin'} placeholder={'Password'} />
+            <label className={style.checkboxLabel}>
+              <input type="checkbox" checked={isShown} onChange={togglePassword}/>
+              <em>Show password?</em>
+            </label>
             <ButtonMedium className={style.formOptions} text={'delete'} width={''}/>
           </form>
         </ContainerBox>
--- frontend/src/views/Signup/Signup.js
+++ frontend/src/views/Signup/Signup.js
@@ -6,9 +6,15 @@ import Input from '@components/Input/Input';
 import ButtonMedium from '@components/Buttons/ButtonMedium';
 import logo from '@resources/img/logoVertical.svg';
 import { Link } from 'react-router-dom';
-
+import { useState } from 'react';
 
 function Signup() {
+  const [isShown, setIsSHown] = useState(false);
+
+  const togglePassword = () => {
+    setIsSHown((isShown) => !isShown);
+  };
+
   return (
     <Page titlebar={false}>
       <section className={style.section}>
@@ -19,10 +25,16 @@ function Signup() {
           <form method="post" action="#" className={style.form} name={'signup'}>
             <Input type={'text'} name={'usernameLog'} placeholder={'Username'} required className={style.formElement}/>
             <Input type={'email'} name={'emailLog'} placeholder={'Address email'} required className={style.formElement}/>
-            <Input type={'password'} name={'passwordLog'} placeholder={'Password'} required className={style.formElement}/>
-            <Input type={'password'} name={'passwordRepeatLog'} placeholder={'Repeat your password'} required className={style.formElement}/>
+            <Input type={isShown ? 'text' : 'password'} name={'passwordRegister'} placeholder={'Password'} required className={style.formElement}/>
+            <Input type={isShown ? 'text' : 'password'} name={'passwordRegisterRepeat'}
+              placeholder={'Repeat your password'} required className={style.formElement}/>
+            <label className={style.checkboxLabel}>
+              <input type="checkbox" checked={isShown} onChange={togglePassword}/>
+              <em>Show password?</em>
+            </label>
             <label className={style.label}>
-              <input type="checkbox" name="terms" value="terms" required={true}/><em>I agree to our <Link to={'/privacy'}>privacy and terms of service</Link>.</em>
+              <input type="checkbox" name="terms" value="terms" required={true}/>
+              <em>I agree to our <Link to={'/privacy'}>privacy and terms of service</Link>.</em>
             </label>
 
             <div className={style.formOptions}>
