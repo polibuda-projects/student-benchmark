0a03aca14585df5f5ce0964e9752ee5facdb4c76
wojzam <wojciech.zamarski@student.pk.edu.pl>
2022-12-11 13:10:16 +0100

    b added ChangeUserPasswordController tests

---
+++ src/test/java/com/example/studentbenchmark/entity/TestConstants.java
@@ -0,0 +1,22 @@
+package com.example.studentbenchmark.entity;
+
+import com.google.gson.JsonObject;
+
+public class TestConstants {
+
+    public static final String USER_NICKNAME = "nickname";
+    public static final String USER_EMAIL = "email@email.com";
+    public static final String USER_PASSWORD = "P@ssw0rd";
+    public static final String USER_NEW_PASSWORD = "P@ssw0rdNEW";
+
+    public static final JsonObject CHANGE_USER_PASSWORD_REQUEST = createChangeUserPasswordRequest();
+
+    private static JsonObject createChangeUserPasswordRequest() {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("oldPassword", USER_PASSWORD);
+        jsonObject.addProperty("newPassword", USER_NEW_PASSWORD);
+        jsonObject.addProperty("newPasswordRepeated", USER_NEW_PASSWORD);
+        return jsonObject;
+    }
+
+}
