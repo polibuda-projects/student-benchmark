03d28ce835c2d6732106fa878cec16000f42e35b
Nikodem Zbaraza <nikozet@interia.pl>
2022-12-12 17:12:19 +0100

    b removed shitty code

--- src/test/java/com/example/studentbenchmark/controllers/DashboardControllerTest.java
+++ src/test/java/com/example/studentbenchmark/controllers/DashboardControllerTest.java
@@ -23,66 +23,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.List;
 
 @SpringBootTest
-class DashboardControllerTest<T> {
-    // Wstrzyknij tutaj potrzebne zależności
-    private NumberTestRepo numberTestRepo;
-    private SequenceTestRepo sequenceTestRepo;
-    private VerbalTestRepo verbalTestRepo;
-    private VisualTestRepo visualTestRepo;
+class DashboardControllerTest {
 
-
-    @Autowired
-    private DashboardController dashboardController;
-
-    @BeforeEach
-    void setUp() {
-        // Tutaj możesz wstawić kod, który wykona się przed każdym testem,
-        // np. ustawienie wartości pól klasowych
-    }
-
-
-        @Test
-        public void testGetDashboardPublicData() {
-            // given
-            // Wygenerowanie przykładowych danych testowych
-            List<NumberTest> numberBestList = numberTestRepo.findBestScores();
-            List<SequenceTest> sequenceBestList =  sequenceTestRepo.findBestScores();
-            List<VerbalTest> verbalBestList = verbalTestRepo.findBestScores();
-            List<VisualTest> visualBestList = visualTestRepo.findBestScores();
-
-            List<NumberTest> numberGraphList = numberTestRepo.findAllGraphValidScores();
-            List<SequenceTest> sequenceGraphList = sequenceTestRepo.findAllGraphValidScores();
-            List<VerbalTest> verbalGraphList = verbalTestRepo.findAllGraphValidScores();
-            List<VisualTest> visualGraphList = visualTestRepo.findAllGraphValidScores();
-
-            // Ustawienie mocków repository
-            when(numberTestRepo.findBestScores()).thenReturn(numberBestList);
-            when(sequenceTestRepo.findBestScores()).thenReturn(sequenceBestList);
-            when(verbalTestRepo.findBestScores()).thenReturn(verbalBestList);
-            when(visualTestRepo.findBestScores()).thenReturn(visualBestList);
-
-            when(numberTestRepo.findAllGraphValidScores()).thenReturn(numberGraphList);
-            when(sequenceTestRepo.findAllGraphValidScores()).thenReturn(sequenceGraphList);
-            when(verbalTestRepo.findAllGraphValidScores()).thenReturn(verbalGraphList);
-            when(visualTestRepo.findAllGraphValidScores()).thenReturn(visualGraphList);
-
-            // when
-            String result = dashboardController.getDashboardPublicData();
-
-            // then
-            // Sprawdzenie czy zwracany obiekt jest listą
-            //assertThat(result, instanceOf(List.class));
-            assertEquals(result, "dashboard");
-
-            // Sprawdzenie czy lista zawiera poprawne dane
-            //assertThat(result, contains(numberBestList, sequenceBestList, verbalBestList, visualBestList, numberGraphList, sequenceGraphList, verbalGraphList, visualGraphList));
-        }
-
-
-    @Test
-    void testGetDashboardPersonalData() {
-        // Tutaj możesz napisać test dla metody getDashboardPersonalData()
-        // wykorzystując przy tym zależności wstrzykiwane do klasy
-
-    }
 }
