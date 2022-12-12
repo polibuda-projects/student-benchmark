9b380730ef6f2b5aab3970c07c166d482bfc75b1
Nikodem Zbaraza <nikozet@interia.pl>
2022-12-12 11:12:43 +0100

    b changed format to json

--- src/main/java/com/example/studentbenchmark/controllers/AdminDashboardController.java
+++ src/main/java/com/example/studentbenchmark/controllers/AdminDashboardController.java
@@ -5,6 +5,7 @@ import com.example.studentbenchmark.entity.LoggerEntity;
 import com.example.studentbenchmark.entity.SupportMessage;
 import com.example.studentbenchmark.repository.LogsRepo;
 import com.example.studentbenchmark.repository.SupportRepo;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -28,28 +30,30 @@ public class AdminDashboardController {
     }
 
     @GetMapping("/adminDashboard/logs")
-    public List<LoggerEntity> adminDashboardLogs() {
+    public String adminDashboardLogs() {
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         AppUserEntityDetails currentUser = (AppUserEntityDetails) authentication.getPrincipal();
 
         if (currentUser.getAuthorities().toString().equals("[ROLE_ADMIN]")) {
             List<LoggerEntity> log = logsRepo.findAll();
-            return log;
+            Gson gson = new Gson();
+            return gson.toJson(log);
         } else {
             throw new RuntimeException("You are not authorized to view this page");
         }
     }
 
     @GetMapping("/adminDashboard/messages")
-    public List<SupportMessage> adminDashboardMessages() {
+    public String adminDashboardMessages() {
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         AppUserEntityDetails currentUser = (AppUserEntityDetails) authentication.getPrincipal();
 
         if (currentUser.getAuthorities().toString().equals("[ROLE_ADMIN]")) {
             List<SupportMessage> messages = supportRepo.findAll();
-            return messages;
+            Gson gson = new Gson();
+            return gson.toJson(messages);
         } else {
             throw new RuntimeException("You are not authorized to view this page");
         }
