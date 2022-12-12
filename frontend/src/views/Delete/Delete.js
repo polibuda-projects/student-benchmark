5667e9b7f64bcc80186c975c6a02477f081f974b
Konrad Wojtowicz <konrad.wojtowicz@interia.pl>
2022-12-12 20:15:15 +0100

    f added mobile version of delete page

--- frontend/src/views/Delete/Delete.js
+++ frontend/src/views/Delete/Delete.js
@@ -19,11 +19,13 @@ function Delete() {
         <ContainerBox width={'60em'} className={style.containerBox}>
           <h1 className={style.title}>Enter your password</h1>
           <form method="post" action="#" className={style.form}>
-            <Input type={isShown ? 'text' : 'password'} name={'passwordLogin'} placeholder={'Password'} />
-            <label className={style.checkboxLabel}>
-              <input type="checkbox" checked={isShown} onChange={togglePassword}/>
-              <em>Show password?</em>
-            </label>
+            <div className={style.passwordAlign}>
+              <Input type={isShown ? 'text' : 'password'} name={'passwordLogin'} placeholder={'Password'} />
+              <label className={style.checkboxLabel}>
+                <input className={style.formElement} type="checkbox" checked={isShown} onChange={togglePassword}/>
+                <em>Show password?</em>
+              </label>
+            </div>
             <ButtonMedium className={style.formOptions} text={'delete'} width={''}/>
           </form>
         </ContainerBox>
