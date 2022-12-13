00d94bcd780904123671430acb4b9b407098dffa
jkbwtk <jakub.wiatek@student.pk.edu.pl>
2022-12-14 00:57:50 +0100

    f integrated login with backend

--- frontend/src/views/Login/Login.js
+++ frontend/src/views/Login/Login.js
@@ -6,13 +6,36 @@ import Input from '@components/Input/Input';
 import logo from '@resources/img/logoVertical.svg';
 import ButtonMedium from '@components/Buttons/ButtonMedium';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+const fetchUrl = `${process.env.REACT_APP_BACKEND_URL}/login`;
 
 function Login() {
   const [isShown, setIsSHown] = useState(false);
 
   const togglePassword = () => {
     setIsSHown((isShown) => !isShown);
+  };
+
+  const nickname = useRef(null);
+  const password = useRef(null);
+
+  async function sendRegisterRequest() {
+    const body = new FormData();
+    body.append('username', nickname.current.value);
+    body.append('password', password.current.value);
+
+    const requestOptions = {
+      method: 'POST',
+      body: body,
+    };
+
+
+    try {
+      await fetch(fetchUrl, requestOptions, { mode: 'cors' });
+      document.location.replace('/');
+    } catch (error) {
+      console.log('Login error');
+    }
   };
 
   return (
@@ -22,15 +45,15 @@ function Login() {
         <ContainerBox width={'60em'}>
           <h1 className={style.title}>Log in</h1>
           <form method="post" action="#" className={style.form}>
-            <Input type={'email'} name={'emailLog'} placeholder={'Address email'} required className={style.formElement}/>
-            <Input type={isShown ? 'text' : 'password'} name={'passwordLogin'} placeholder={'Password'} required className={style.formElement}/>
+            <Input useRef={nickname} type={'text'} name={'usernameLog'} placeholder={'Username'} required className={style.formElement}/>
+            <Input useRef={password} type={isShown ? 'text' : 'password'} name={'passwordLogin'} placeholder={'Password'} required className={style.formElement}/>
             <label className={style.checkboxLabel}>
               <input type="checkbox" checked={isShown} onChange={togglePassword}/>
               <em>Show password?</em>
             </label>
 
             <div className={style.formOptions}>
-              <ButtonMedium text={'Login'} width={''}/>
+              <ButtonMedium onClick={sendRegisterRequest} text={'Login'} width={''}/>
               <div className={style.formOptionsLink}>
                 <Link to='/signup'>Sign up</Link>
                 <Link to='/recover'>Reset my password</Link>
