7376a6c2fa4c953b55ff44128e2bf34396469590
Pawel Wypych <pawelwypych4@gmail.com>
2022-12-16 13:50:22 +0100

    b Changed validation, added checking if password is allowed in register

--- src/main/java/com/example/studentbenchmark/controllers/SupportController.java
+++ src/main/java/com/example/studentbenchmark/controllers/SupportController.java
@@ -60,14 +60,14 @@ public class SupportController {
     }
 
     private record SupportRequest(
-            @NotNull
-            @NotBlank
-            @Size(min = 5, max = 256)
+            @NotNull(message = "0")
+            @NotBlank(message = "1")
+            @Size(min = 5, max = 256, message = "2")
             String message,
 
-            @NotNull
-            @NotBlank
-            @Size(max = 64)
+            @NotNull(message = "0")
+            @NotBlank(message = "1")
+            @Size(min = 3, max = 64, message = "2")
             String messageTitle) {
     }
 }
